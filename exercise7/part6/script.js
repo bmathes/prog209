84663a8076102f47eb4bf0502df85d78745a78cb
bmathes <bmathes@users.noreply.github.com>
2018-02-25 04:55:43 -0800

    Update script.js

--- exercise7/part6/script.js
+++ exercise7/part6/script.js
@@ -20,11 +20,11 @@ $(function() {
             $img.animate({
             // moves the image 600 pixels to the right of its current position
             'margin-left': '+=600'
-            // fadeTo takes two arguments. The first will cause the element to  fade out over a period of 1500 milliseconds
-            // or 1.5 seconds. The second is the opacity which is set to 50%
-            $img.fadeTo(1500, 0.5);                
             // the animation takes 1500 milliseconds and uses the ease transition called easeInBack for a
             // animation effect supplied by the jquery.easing.1.3.js file
             }, 1500, 'easeInBack');
+             // fadeTo takes two arguments. The first will cause the element to  fade out over a period of 1500 milliseconds
+            // or 1.5 seconds. The second is the opacity which is set to 50%
+            $img.fadeTo(1500, 0.5);   
     }))
 });
